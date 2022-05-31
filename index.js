729786b8def6100aa7aa700386b9772ac667461f
Gabriel Feitosa <gabriel.perino99@gmail.com>
2022-05-30 23:50:54 -0300

    feat: key generation

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let prompt = require('prompt-sync')();
 const {generateKeyPair} = require("./utils/publicKeyUtil")
+const {publicKey,privateKey} = generateKeyPair()
 while(true){
     console.log("Bem vindo ao sistema de blockchain!\n")
     let n = prompt("Precione qualquer tecla para continuar: ")
     console.log("Hello!")
+    console.log("Essa é a chave publica: " + publicKey+"\n")
+    console.log("Essa é a chave privada: " + privateKey +"\n")
     n = prompt("Deseja continuar? ")
     if(n == "-1"){
         break
