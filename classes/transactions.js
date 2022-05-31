b3a1b5feea7fe8657a7641ca582f9d2a940c6887
Gabriel Feitosa <gabriel.perino99@gmail.com>
2022-05-31 08:36:09 -0300

    feat: better coments, added isFromMining to transaction

--- classes/transactions.js
+++ classes/transactions.js
@@ -1,35 +1,50 @@
 const {verifySignature,signWithKey} = require("../utils/publicKeyUtil")
 const crypto = require('crypto');
 class Transaction {
-
-    constructor(fromAddress, toAddress, amount) {
+    /**
+     * Método construtor recebe os parâmetros
+     * @param {*} fromAddress 
+     * @param {*} toAddress 
+     * @param {*} amount 
+     */
+    constructor(fromAddress, toAddress, amount,isFromMining=false) {
+      
       this.fromAddress = fromAddress;
       this.toAddress = toAddress;
       this.amount = amount;
       this.timestamp = Date.now();
+      this.isFromMining = isFromMining;
     }
-  
+    /**
+     * Gera o hash da transação usando o algoritmo sha256 
+     * em cima da concatenação do endereço de origem, endereço de destino,
+     * valor da transação e timestamp
+     * @returns {string}
+     */
     calculateHash() {
       return crypto.createHash('sha256').update(this.fromAddress + this.toAddress + this.amount + this.timestamp).digest('hex');
     }
   
     /**
-     * Signs a transaction with the given signingKey (which is an Elliptic keypair
-     * object that contains a private key). The signature is then stored inside the
-     * transaction object and later stored on the blockchain.
+     * Recebe como parâmetro a chave pública gerada pela função de gerar pares de 
+     * chaves
      *
      * @param {string} signingKey
      */
     signTransaction(signingKey) {
-      // You can only send a transaction from the wallet that is linked to your
-      // key. So here we check if the fromAddress matches your publicKey
+    /**  
+     * Verifica se a chave recebida para assinatura é a mesma chave no endeço de
+     * origem
+    */
       if (signingKey !== this.fromAddress) {
-        throw new Error('You cannot sign transactions for other wallets!');
+        throw new Error('Não é possível assinar transactions para outras carteiras!');
       }
       
   
-      // Calculate the hash of this transaction, sign it with the key
-      // and store it inside the transaction object
+ /**
+  * Calcula o hash da transaction, assina o hash utilizando a chave passada por
+  * parâmetro seta a assinatura da classe
+  */
       const hashTx = this.calculateHash();
       const sig = signWithKey(signingKey,hashTx) 
   
@@ -37,19 +52,24 @@ class Transaction {
     }
   
     /**
-     * Checks if the signature is valid (transaction has not been tampered with).
-     * It uses the fromAddress as the public key.
+     * Verifica a validade da transação pelo endereço de origem
+     * O endeço de origem é passado para o módulo de geração de chaves
+     * que irá realizar o trabalho de parsear a string para um objeto de
+     * chave utilizá
      *
      * @returns {boolean}
      */
     isValid() {
-      // If the transaction doesn't have a from address we assume it's a
-      // mining reward and that it's valid. You could verify this in a
-      // different way (special field for instance)
-      if (this.fromAddress === null) return true;
+      /**
+       * Se o endereço de origem for nulo se considera que a transaction é resultado do
+       * provcesso de mineração de bloco
+       * Além disso a propriedade isFromMining deve ser verdadeira para confirmar que a transaction
+       * surgiu do processo de mineração de um novo bloco não da transferência entre dois endereços
+       */
+      if (this.fromAddress === null&&this.isFromMining=== true) return true;
   
       if (!this.signature || this.signature.length === 0) {
-        throw new Error('No signature in this transaction');
+        throw new Error('Essa transação não é confiável, pois não tem assinatura');
       }
   
    
